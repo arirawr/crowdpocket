4e355462c66324728d038ec5769bde4fb854e824
Arielle VanIderstine <arielle1459@gmail.com>
2015-05-03 22:51:34 -0400

    working - get list of tags, push to other user

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from keys import keys
 # AUTH
 ck = keys['consumer_key']
 at = keys['access_token']
+at2 = keys['at2']
+
+tags = "example"
 
 # ACTIONS
-actions = '[{ "action" : "tags_add", "tags" : "test", "item_id" : "18742594"}]'
+
+payload = {'consumer_key': ck, 'access_token': at2, 'tag': tags}
+r = requests.get("https://getpocket.com/v3/get", params=payload)
+json_key = r.json()['list'].keys()[0]
+found_url = r.json()['list'][json_key]['given_url']
+print found_url
+
+actions = '[{ "action" : "add", "tags" : "' + tags + '", "url" : "' + found_url + '"}]'
 
 payload = {'consumer_key': ck, 'access_token': at, 'actions': actions}
 r = requests.post("https://getpocket.com/v3/send", params=payload)
