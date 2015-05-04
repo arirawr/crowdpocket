0b9349e03c09189009774d0240f73cce5e444316
Arielle VanIderstine <arielle1459@gmail.com>
2015-05-03 20:55:19 -0400

    basic requests working

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ ck = keys['consumer_key']
 at = keys['access_token']
 
 # ACTIONS
-actions = [
-	{
-		"action" : "add",
-		"item_id" : "89419281",
-	}
-]
+actions = '[{ "action" : "tags_add", "tags" : "test", "item_id" : "18742594"}]'
 
 payload = {'consumer_key': ck, 'access_token': at, 'actions': actions}
 r = requests.post("https://getpocket.com/v3/send", params=payload)
+print r.text
