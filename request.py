427eeefda2b35bec8121bd4724121faa5c988f9c
Arielle VanIderstine <arielle1459@gmail.com>
2015-05-05 12:21:57 -0400

    working for more than 2 users

---
+++ request.py
@@ -0,0 +1,40 @@
+import requests
+from keys import keys
+
+# AUTH
+ck = keys['consumer_key']
+#at = keys['access_token']
+#ari = keys['ari']
+#zak = keys['zak']
+users = keys['group_users']
+
+tags = "nonexistent"
+
+# ACTIONS
+
+actions = '['
+
+for user in users:
+	payload = {'consumer_key': ck, 'access_token': user, 'tag': tags}
+	r = requests.get("https://getpocket.com/v3/get", params=payload)
+	if r.json()['list']:
+		json_key = r.json()['list'].keys()
+		for key in json_key:
+			found_url = r.json()['list'][key]['given_url']
+			action = '{ "action" : "add", "tags" : "' + tags + '", "url" : "' + found_url + '"},'
+			actions += action
+actions = actions[:-1]
+actions += ']'
+print actions
+
+#actions = '[{ "action" : "add", "tags" : "' + tags + '", "url" : "' + found_url + '"}]'
+
+#actions = '[{ "action" : "add", "tags" : "zakari", "url" : "http://google.com"},{ "action" : "add", "tags" : "zakari", "url" : "http://mcgill.ca"}]'
+
+if actions != ']':
+	for user in users:
+		payload = {'consumer_key': ck, 'access_token': user, 'actions': actions}
+		r = requests.post("https://getpocket.com/v3/send", params=payload)
+		print r.text
+else: 
+	print "no actions"
