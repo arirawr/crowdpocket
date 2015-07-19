e14f79d444a53286125372010b2c9887523fb3a2
Arielle VanIderstine <arielle1459@gmail.com>
2015-07-18 18:52:18 -0700

    refactoring for more comprehensive key storage

--- request.py
+++ request.py
@@ -1,20 +1,22 @@
 import datetime
 import requests
-from keys import keys
+from keys import keys, userkeys, groups
 
 # AUTH
 ck = keys['consumer_key']
-users = keys['group_users']
 
-tags = ['bae']
+groupname = 'bae'
+group = groups[groupname]
+members = group['members']
+tags = group['tags']
 
 # ACTIONS
 
 actions = '['
 
-for user in users:
+for member in members:
 	for tag in tags:
-		payload = {'consumer_key': ck, 'access_token': user, 'tag': tag}
+		payload = {'consumer_key': ck, 'access_token': userkeys[member], 'tag': tag}
 		r = requests.get("https://getpocket.com/v3/get", params=payload)
 		if r.json()['list']:
 			json_key = r.json()['list'].keys()
@@ -32,8 +34,8 @@ print actions
 #actions = '[{ "action" : "add", "tags" : "zakari", "url" : "http://google.com"},{ "action" : "add", "tags" : "zakari", "url" : "http://mcgill.ca"}]'
 
 if actions != ']':
-	for user in users:
-		payload = {'consumer_key': ck, 'access_token': user, 'actions': actions}
+	for member in members:
+		payload = {'consumer_key': ck, 'access_token': userkeys[member], 'actions': actions}
 		r = requests.post("https://getpocket.com/v3/send", params=payload)
 		print r.text
 else: 
