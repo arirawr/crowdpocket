aab8dc1715c8e77928ccc4908b541cd41a42b50d
Arielle VanIderstine <hack@ariari.io>
2015-07-18 19:20:32 -0700

    Create fakekeys.py

---
+++ fakekeys.py
@@ -0,0 +1,26 @@
+userkeys = dict(
+    user1 = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxx",
+    user2 = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxx",
+    user3 = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxx",
+)
+
+groups = dict(
+    group1 = dict(
+        tags = {
+            'tag1', 'tag2'
+        }
+        members = {
+            'user1', 'user2'
+        }
+    ),
+    group2 = dict(
+        tags = 'tag3',
+        members = {
+            'user3', 'user1', 'user2' 
+        }   
+    )
+)
+
+keys = dict(
+    consumer_key = "xxxxx-xxxxxxxxxxxxxxxxxxxxxxxx",
+)
