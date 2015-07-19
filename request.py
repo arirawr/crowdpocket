ed9ee04860a96fa69565db906a2c90e255d9b528
Arielle VanIderstine <arielle1459@gmail.com>
2015-07-18 19:09:51 -0700

    support for multiple groups

--- request.py
+++ request.py
@@ -2,41 +2,41 @@ import datetime
 import requests
 from keys import keys, userkeys, groups
 
-# AUTH
+# App key
 ck = keys['consumer_key']
 
-groupname = 'bae'
-group = groups[groupname]
-members = group['members']
-tags = group['tags']
+def update(groupname):
 
-# ACTIONS
+	group = groups[groupname]
+	members = group['members']
+	tags = group['tags']
 
-actions = '['
-
-for member in members:
-	for tag in tags:
-		payload = {'consumer_key': ck, 'access_token': userkeys[member], 'tag': tag}
-		r = requests.get("https://getpocket.com/v3/get", params=payload)
-		if r.json()['list']:
-			json_key = r.json()['list'].keys()
-			for key in json_key:
-				found_url = r.json()['list'][key]['given_url']
-				action = '{ "action" : "add", "tags" : "' + tag + '", "url" : "' + found_url + '"},'
-				actions += action
-actions = actions[:-1]
-actions += ']'
-print datetime.datetime.now()
-print actions
-
-#actions = '[{ "action" : "add", "tags" : "' + tags + '", "url" : "' + found_url + '"}]'
-
-#actions = '[{ "action" : "add", "tags" : "zakari", "url" : "http://google.com"},{ "action" : "add", "tags" : "zakari", "url" : "http://mcgill.ca"}]'
-
-if actions != ']':
+	# ACTIONS
+	
+	actions = '['
+	
 	for member in members:
-		payload = {'consumer_key': ck, 'access_token': userkeys[member], 'actions': actions}
-		r = requests.post("https://getpocket.com/v3/send", params=payload)
-		print r.text
-else: 
-	print "no actions"
+		for tag in tags:
+			payload = {'consumer_key': ck, 'access_token': userkeys[member], 'tag': tag}
+			r = requests.get("https://getpocket.com/v3/get", params=payload)
+			if r.json()['list']:
+				json_key = r.json()['list'].keys()
+				for key in json_key:
+					found_url = r.json()['list'][key]['given_url']
+					action = '{ "action" : "add", "tags" : "' + tag + '", "url" : "' + found_url + '"},'
+					actions += action
+	actions = actions[:-1]
+	actions += ']'
+	print datetime.datetime.now()
+	print actions
+	
+	if actions != ']':
+		for member in members:
+			payload = {'consumer_key': ck, 'access_token': userkeys[member], 'actions': actions}
+			r = requests.post("https://getpocket.com/v3/send", params=payload)
+			print r.text
+	else: 
+		print "no actions"
+		
+for group in groups:
+	update(group)
